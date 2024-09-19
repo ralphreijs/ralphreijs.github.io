c9c0529081618fa6cea32a82fe8bb7919aea3aea
ralphreijs <ralph.simon.reijs@gmail.com>
2024-09-19 13:36:16 +0200

    Cleaning up

--- assets/js/service-worker.js
+++ assets/js/service-worker.js
@@ -29,7 +29,7 @@ const SHELL_CACHE = "shell-9.2.1--v13--sw/";
 const ASSETS_CACHE = "assets--v13--sw/";
 
 // The cache for regular content, which will be invalidated every time you make a new build.
-const CONTENT_CACHE = "content--2024-09-19T12:12:57+02:00--sw/";
+const CONTENT_CACHE = "content--2024-09-19T13:35:36+02:00--sw/";
 
 // A URL search parameter you can add to external assets to cache them in the service worker.
 const SW_CACHE_SEARCH_PARAM = "sw-cache";
@@ -52,7 +52,6 @@ const SHELL_FILES = [
 ];
 
 const STATIC_FILES = [
-  /**/"/README.md",
   /**/"/assets/Reijs_Ralph.vcf",
   /**/"/assets/bower.json",
   /**/"/assets/bower_components/MathJax/LICENSE",
