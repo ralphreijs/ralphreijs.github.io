5f469073f24858f6a5d26f49bf89aab382bacb65
ralphreijs <ralph.simon.reijs@gmail.com>
2024-09-27 23:19:19 +0200

    Apostille USA Updates

--- assets/js/sw.js
+++ assets/js/sw.js
@@ -29,7 +29,7 @@ const SHELL_CACHE = "shell-9.2.1--v13--sw/";
 const ASSETS_CACHE = "assets--v13--sw/";
 
 // The cache for regular content, which will be invalidated every time you make a new build.
-const CONTENT_CACHE = "content--2024-09-27T22:39:56+02:00--sw/";
+const CONTENT_CACHE = "content--2024-09-27T23:19:02+02:00--sw/";
 
 // A URL search parameter you can add to external assets to cache them in the service worker.
 const SW_CACHE_SEARCH_PARAM = "sw-cache";
@@ -450,6 +450,7 @@ const STATIC_FILES = [
   /**/"/assets/img/projects/projects_anima_full_10.jpg",
   /**/"/assets/img/projects/projects_apostilleusa.jpg",
   /**/"/assets/img/projects/projects_apostilleusa_full_01.jpg",
+  /**/"/assets/img/projects/projects_apostilleusa_full_02.jpg",
   /**/"/assets/img/projects/projects_app_appcodex_full_01.jpg",
   /**/"/assets/img/projects/projects_app_appcodex_full_02.jpg",
   /**/"/assets/img/projects/projects_app_astroweather_full_01.jpg",
