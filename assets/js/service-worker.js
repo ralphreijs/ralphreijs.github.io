ae909363f459b2fc9b26c24969c024f1b649b302
ralphreijs <ralph.simon.reijs@gmail.com>
2024-11-11 08:37:05 +0100

    Minor fixes to AI pages

--- assets/js/service-worker.js
+++ assets/js/service-worker.js
@@ -29,7 +29,7 @@ const SHELL_CACHE = "shell-9.2.1--v13--sw/";
 const ASSETS_CACHE = "assets--v13--sw/";
 
 // The cache for regular content, which will be invalidated every time you make a new build.
-const CONTENT_CACHE = "content--2024-11-11T08:27:53+01:00--sw/";
+const CONTENT_CACHE = "content--2024-11-11T08:36:27+01:00--sw/";
 
 // A URL search parameter you can add to external assets to cache them in the service worker.
 const SW_CACHE_SEARCH_PARAM = "sw-cache";
