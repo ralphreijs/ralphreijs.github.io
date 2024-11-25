94d4786162863b767403446c6f7b66f8e8f49d6f
ralphreijs <ralph.simon.reijs@gmail.com>
2024-11-25 09:52:45 +0100

    Textual corrections

--- assets/js/service-worker.js
+++ assets/js/service-worker.js
@@ -29,7 +29,7 @@ const SHELL_CACHE = "shell-9.2.1--v13--sw/";
 const ASSETS_CACHE = "assets--v13--sw/";
 
 // The cache for regular content, which will be invalidated every time you make a new build.
-const CONTENT_CACHE = "content--2024-11-25T09:47:51+01:00--sw/";
+const CONTENT_CACHE = "content--2024-11-25T09:52:11+01:00--sw/";
 
 // A URL search parameter you can add to external assets to cache them in the service worker.
 const SW_CACHE_SEARCH_PARAM = "sw-cache";
